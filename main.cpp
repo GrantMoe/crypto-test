7b6f8a5d38ebff0ffa6a559a5eac2a64c880d51a
Grant Moe <grant.edward.moe@gmail.com>
2018-07-15 16:03:18 -0700

    philosophy for success: commit late and rarely. Everything is implemented minus threading

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 int main(int argc, char* argv[]) {
 
+    // process arguments
     std::string keyFile;
-
-    for (int i = 1; i < argc; ++i) {
+    for (int i = 1; i < argc; i++) {
         if (std::string(argv[i]) == "-k") {
             if (i + 1 < argc) {
                 keyFile = argv[++i];
@@ -18,27 +17,69 @@ int main(int argc, char* argv[]) {
         // thread count argument handling goes here
     }
 
-    std::streampos size;
-    char * memblock;
-    std::ifstream file (keyFile, std::ios::in|std::ios::binary|std::ios::ate);
 
-    if (file.is_open())
-    {
-        size = file.tellg();
-        memblock = new char [size];
-        file.seekg (0, std::ios::beg);
-        file.read (memblock, size);
+    // open key file
+    std::ifstream keyStream (keyFile, std::ios::in|std::ios::binary|std::ios::ate);
 
-        file.close();
+    if (keyStream.is_open()) {
 
-        std::cout << memblock;
+        char * keyBytes;
+        char * plainBytes;
+        char * cypherBytes;
 
-        std::cout << "\n\nthe entire file content is in memory\n\n";
+        // get key bytes and chunk size
+        std::streampos chunkSize = keyStream.tellg();
+        keyBytes = new char [chunkSize];
+        plainBytes = new char [chunkSize];
+        cypherBytes = new char [chunkSize];
+        keyStream.seekg (0, std::ios::beg);
+        keyStream.read (keyBytes, chunkSize);
+        keyStream.close();
 
-        delete[] memblock;
+        int lastByte;  // count for times when plain data is not a multiple of chunk size
+
+        // open stdin and stdout streams
+        if (std::freopen("plain", "r", stdin)) { // CLion doesn't support command line redirects in debugging!
+            std::freopen("cypher", "w", stdout);  // C'mon Jetbrains. Get it together.
+
+            while (std::cin.good()) {
+
+                // get chunk of plain data
+                lastByte = 0;
+                for (int i = 0; i < chunkSize; i++) {
+                    if (std::cin.get(plainBytes[i])) {
+                        lastByte++;
+                    } else { // if there are fewer than chunkSize bytes in the last bit of plain data:
+                        break;
+                    }
+                }
+
+                // XOR-encrypt chunk with key
+                if (!std::cin.eof()) {
+                    for (int i = 0; i < lastByte; i++) {
+                        cypherBytes[i] = plainBytes[i] ^ keyBytes[i];
+                    }
+
+//              write cyphered chunk to stdout
+                    for (int i = 0; i < lastByte; i++) {
+                        std::cout << cypherBytes[i];
+                    }
+                }
+            }
+
+            // close streams
+            std::fclose(stdin);
+            std::fclose(stdout);
+        }
+
+        delete[] keyBytes;
+        delete[] plainBytes;
+        delete[] cypherBytes;
+
+    } else {
+        std::cerr << "cannot get key";
+        return 1;
     }
-
-    else std::cout << "Unable to open file";
 
     return 0;
 }
